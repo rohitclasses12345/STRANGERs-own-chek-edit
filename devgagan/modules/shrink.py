6dfaf479171c6f96544975368d55cf0d67a408fd
Kumarrohit92572 <rjpdf82278@gmail.com>
2025-10-02 01:58:53 +0530

    Update shrink.py

--- devgagan/modules/shrink.py
+++ devgagan/modules/shrink.py
@@ -73,9 +73,32 @@ async def token_handler(client, message):
     msg = await app.get_messages(chat_id, 796)
     user_id = message.chat.id
     if len(message.command) <= 1:
-        image_url = "https://i.ibb.co/ccV44ZRS/STRANGER-BOY.jpg"
-        join_button = InlineKeyboardButton("Join Channel", url="https://t.me/Strangerboys24")
-        premium = InlineKeyboardButton("Get Premium", url="https://t.me/Strangerboys24")   
+        # List of multiple starting images
+        start_images = [
+            "https://i.ibb.co/ZRvCxd5g/STRANGER-BOY.jpg",
+            "https://www.freepik.com/free-ai-image/lifestyle-scene-with-people-doing-regular-tasks-anime-style_94952596.htm#fromView=keyword&page=2&position=20&uuid=df9b46bb-3e9a-43e9-b3bb-481ac83cbced&query=Sexy+anime+girl",
+            "https://www.freepik.com/free-ai-image/anime-style-illustration-rose_206341678.htm#fromView=keyword&page=1&position=19&uuid=f5070f8a-bf67-4774-86c7-92067ce73da6&query=Sexy+anime+girl",
+            "https://i.ibb.co/9HhNmHYg/STRANGER-BOY.jpg",
+            "https://www.freepik.com/premium-ai-image/modern-spirit-fashion-forward-diva_358241706.htm#fromView=keyword&page=1&position=29&uuid=f5070f8a-bf67-4774-86c7-92067ce73da6&query=Sexy+anime+girl",
+            "https://i.ibb.co/JwwmgFs6/STRANGER-BOY.jpg",
+            "https://www.freepik.com/premium-ai-image/most-beautiful-woman-history-kawaii-soft-smile-anime_166751376.htm#fromView=keyword&page=2&position=14&uuid=df9b46bb-3e9a-43e9-b3bb-481ac83cbced&query=Sexy+anime+girl",
+            "https://www.freepik.com/premium-ai-image/striking-anime-girl-with-fiery-red-hair-captivating-blue-eyes_339512292.htm#fromView=keyword&page=2&position=5&uuid=df9b46bb-3e9a-43e9-b3bb-481ac83cbced&query=Sexy+anime+girl",
+            "https://www.freepik.com/free-ai-image/portrait-anime-character-doing-fitness-exercising_236197782.htm#fromView=search&page=2&position=22&uuid=7aca9fb5-c87b-4cce-a2bd-8a697e666bc0&query=Hot+girl+cute+animated",
+            "https://www.freepik.com/premium-ai-image/stunning-digital-portrait-confident-female-with-expressive-blue-eyes-flowing-black-hair_292912268.htm#fromView=search&page=1&position=19&uuid=05f5aa6f-d09a-4578-9972-302056830492&query=Hot+girl+cute+animated",
+            "https://i.ibb.co/ccV44ZRS/STRANGER-BOY.jpg",
+            "https://www.freepik.com/premium-ai-image/anime-girl-sitting-window-sill-with-blue-eyes-black-dress_402567199.htm#fromView=keyword&page=1&position=14&uuid=f5070f8a-bf67-4774-86c7-92067ce73da6&query=Sexy+anime+girl",
+            "https://i.ibb.co/7xm7cXyg/STRANGER-BOY.jpg",
+            "https://www.freepik.com/premium-ai-image/anime-girl-sitting-desk-with-cell-phone-her-hand_313114443.htm#fromView=keyword&page=1&position=15&uuid=f5070f8a-bf67-4774-86c7-92067ce73da6&query=Sexy+anime+girl",
+            "https://i.ibb.co/q3SX4gjQ/STRANGER-BOY.jpg",
+            "https://www.freepik.com/premium-ai-image/drawing-woman-blue-dress_59951677.htm#fromView=keyword&page=1&position=17&uuid=f5070f8a-bf67-4774-86c7-92067ce73da6&query=Sexy+anime+girl",
+            "https://i.ibb.co/0p3pmkwn/Angel.jpg",
+            "https://www.freepik.com/free-ai-image/anime-like-illustration-girl-portrait_351801839.htm#fromView=keyword&page=1&position=16&uuid=f5070f8a-bf67-4774-86c7-92067ce73da6&query=Sexy+anime+girl",
+         ]
+
+        # Pick random image
+        image_url = random.choice(start_images)
+        join_button = InlineKeyboardButton("OWNER Channel", url="https://t.me/Strangerboys24")
+        premium = InlineKeyboardButton("Get Premium  OWNER", url="https://t.me/Strangerboys24")   
         keyboard = InlineKeyboardMarkup([
             [join_button],   
             [premium]    
@@ -84,10 +107,23 @@ async def token_handler(client, message):
         await message.reply_photo(
             msg.photo.file_id,
             caption=(
-                "Hi 👋 Welcome, Wanna intro...?\n\n"
-                "Welcome to STRANGER SRC BOT!.../n"
-                "✳️ I can save posts from channels or groups where forwarding is off. I can download videos/audio from YT, INSTA, ... social platforms\n"
-                "✳️ Simply send the post link of a public channel. For private channels, do /login. Send /help to know more."
+                "Hi 👋 Welcome to [STRANGER SRC](https://i.ibb.co/HLNJR68s/x.jpg) BOT!...\n\n"
+                "░░░░░░░░░░░░░░░░░░░░░░\n"
+                "      👑 STRANGER SRC BOT 👑\n"
+                "░░░░░░░░░░░░░░░░░░░░░░\n\n"
+                "👋 Hello Explorer! Welcome to the Quito Edition 🚀\n\n"
+                "╔═════ ✦ FEATURES ✦ ═════╗\n"
+                "🔒 Crack 🔐 Forward-Locked Posts\n"
+                "🎬 HD Video / Audio Grabber (YT, TG)\n"
+                "📸 Insta & Social Media Extractor 🌍\n"
+                "⚡ Ultra-Fast Public Channel Grab\n"
+                "🔐 Private Channels → /login\n"
+                "📜 Full Guide → /help\n"
+                "╚═════════════════════════╝\n\n"
+                "💎 ✦ PREMIUM ZONE ✦ 💎\n"
+                "🚀 Turbo Speed | 📂 Bulk Grab | 🛡️ VIP Support\n"
+                "━━━━━━━━━━━━━━━━━━━━━━━━━\n"
+                "✨ Step inside the STRANGER FAMILY 🔱\n"
             ),
             reply_markup=keyboard
         )
